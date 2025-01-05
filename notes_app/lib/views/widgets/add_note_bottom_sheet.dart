908e6835256b61593a945331e7a3e811ccde663e
A1aaSayed <alaa02.sayed02@gmail.com>
2025-01-05 02:41:39 +0200

    add show modal bottom sheet

---
+++ notes_app/lib/views/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: Text('Alaa Sayed'),
+    );
+  }
+}
