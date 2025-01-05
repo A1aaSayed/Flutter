be489b2453ea36b65bc017c6b1878d75a44cd378
A1aaSayed <alaa02.sayed02@gmail.com>
2025-01-05 02:09:22 +0200

    create notes list view

--- notes_app/lib/views/widgets/custom_note_item.dart
+++ notes_app/lib/views/widgets/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({
@@ -9,6 +10,7 @@ class CustomNoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      margin: EdgeInsets.only(bottom: 20),
       decoration: BoxDecoration(
         color: const Color(0xffffcc80),
         borderRadius: BorderRadius.circular(16),
--- notes_app/lib/views/widgets/notes_view_body.dart
+++ notes_app/lib/views/widgets/notes_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
 import 'custom_note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -12,15 +12,18 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
       child: Column(
-        children: const [
+        children: [
           CustomAppBar(),
           SizedBox(
             height: 20,
           ),
-          CustomNoteItem(),
+          Expanded(
+            child: NotesListView(),
+          )
         ],
       ),
     );
   }
 }
+
 
